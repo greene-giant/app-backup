8b325b1ac8279229b38d7983fe87227e6d1ce03d
Greene Giant <greene.giant.github@gmail.com>
2017-01-01 13:22:46 -0800

    Added in title card and test bindings.

--- src/gui/cards-vsw.py
+++ src/gui/cards-vsw.py
@@ -8,6 +8,30 @@ sticky_all = (tk.N, tk.S, tk.E, tk.W)
 
 
 
+class TitleCard(tk.Frame):
+    def __init__(self, *arg, text, color, **kwargs):
+        # Call original __init__:
+        tk.Frame.__init__(self, *arg, **kwargs)
+
+        # Add vertical bar:
+        bar = tk.Frame(self,
+                       background=color,
+                       width=CARD_BAR_WIDTH)
+        bar.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Add title label:
+        label = tk.Label(self, 
+                         text=text,
+                         foreground=color,
+                         background=COLOR_CARD,
+                         font=(CARD_TITLE_FONT, CARD_TITLE_SIZE),
+                         anchor=tk.CENTER,
+                         pady=CARD_TITLE_PADDING)
+        label.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+
+
+
 class CardHolder(vsw.VerticalScrolledFrame):
     def __init__(self, *arg, **kwargs):
         # Call original __init__:
@@ -35,30 +59,29 @@ class CardHolder(vsw.VerticalScrolledFrame):
                             width=CARD_PADDING_X)
         paddingR.pack(side=tk.LEFT, fill=tk.Y, expand=False)
 
+
         # Add initial vertical padding:
         padding = tk.Frame(self.content,
                            height=CARD_PADDING_Y,
                            background=COLOR_PADDING)
         padding.pack(fill=tk.X)
 
-    def add_title_card(self, text, color):
-        label = tk.Label(self.content,
-                         text=text,
-                         foreground=color,
-                         background=COLOR_CARD,
-                         font=("DejaVu Sans Mono", 16),
-                         anchor=tk.CENTER,
-                         pady=CARD_TITLE_PADDING)
-        label.pack(fill=tk.X)
 
+    def add_card_spacing(self):
         padding = tk.Frame(self.content,
                            height=CARD_PADDING_Y,
                            background=COLOR_PADDING)
         padding.pack(fill=tk.X)
 
+
+
+    def add_title_card(self, text, color):
+        titleCard = TitleCard(self.content, text=text, color=color)
+        titleCard.pack(fill=tk.X)
+
+        self.add_card_spacing()
         self.move_scrollbar_to_bottom()
 
-        
 
 
 
@@ -75,7 +98,13 @@ class TestApp(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        self.bind('a', lambda e: cardHolder.add_title_card("Insert Text", COLOR_COPY))
+        self.bind('a', 
+                lambda e, t="Starting Backup", c=COLOR_COPY: 
+                cardHolder.add_title_card(t, c))
+
+        self.bind('d', 
+                lambda e, t="Backup Complete", c=COLOR_COPY: 
+                cardHolder.add_title_card(t, c))
 
 
 if __name__ == "__main__":
--- src/gui/settings.py
+++ src/gui/settings.py
@@ -7,7 +7,10 @@ COLOR_CARD    = color.wombat.black
 COLOR_PADDING = color.wombat.grayDark
 
 CARD_PADDING_X = 8
-CARD_PADDING_Y = 8
+CARD_PADDING_Y = 10
 CARD_BAR_WIDTH = 10
+
 CARD_TITLE_PADDING = 20
+CARD_TITLE_FONT = "DejaVu Sans Mono"
+CARD_TITLE_SIZE = 16
 
